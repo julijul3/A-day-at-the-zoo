a9389024f3a831deaccd1d28e8e6de351a195590
julijul3 <s212155@student.dtu.dk>
2022-03-30 18:55:58 +0200

    half of part 9

--- 17_02170DatabaseScript2_2022.sql
+++ 17_02170DatabaseScript2_2022.sql
@@ -25,3 +25,38 @@ HAVING COUNT(zoo_id) > 2;
 UPDATE time_slot 
 SET start_time = '13:00', end_time = '14:00'
 WHERE time_slot_ID = '2A';
+
+# part 9
+
+# function that returns the number of caretakers for one enclosure
+DROP FUNCTION IF EXISTS numberCaretaker;
+DELIMITER //
+CREATE FUNCTION numberCaretaker(vEnclosureId int) RETURNS int
+BEGIN
+	DECLARE vCaretakerCount int;
+    SELECT COUNT(*) INTO vCaretakerCount FROM caretaker
+    WHERE vEnclosureId = enclosure_ID;
+    RETURN vCaretakerCount;
+END; //
+DELIMITER ;
+
+SELECT numberCaretaker(21);
+
+# create procedure to add a new show
+
+DROP PROCEDURE IF EXISTS addShow;
+DELIMITER //
+Create Procedure addShow 
+(IN vshow_ID int, IN vanimal_ID int, IN vzoo_Id int, IN vtimeslotID VARCHAR(2), vorganizer_ID int)
+BEGIN
+	INSERT shows(show_ID, animal_ID, zoo_ID, time_slot_ID) 
+    VALUES (vshow_ID, vanimal_ID, vzoo_Id, vtimeslotID);
+    INSERT organizer(employee_ID, show_ID)
+    VALUES(vorganizer_ID, vshow_ID);
+END//
+DELIMITER ;
+
+CALL addShow(4, 21, 1, "2A", 28);
+
+
+
